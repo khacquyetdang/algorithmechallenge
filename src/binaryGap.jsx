692a7efafacc727a43cb1e49348f2b878b52b665
mrdang <khacquyet.dang@gmail.com>
2017-10-21 09:11:13 +0200

    try to optimize binary gap max zero sequence

--- src/binaryGap.jsx
+++ src/binaryGap.jsx
@@ -1,43 +1,36 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function dec2bin(dec){
-    return (dec >>> 0).toString(2);
-}
-
 export default function binaryGap(N) {
     // write your code in JavaScript (Node.js 6.4.0)
     //return 0;
-    var binaryNumber = dec2bin(N);
-    const  findGap = (index, aBinaryNumber, target) => {
+    var binaryNumber = N.toString(2);
+    const findGap = (index, aBinaryNumber, target) => {
         var gapIndex = index;
-        while (gapIndex + 1 < aBinaryNumber.length && aBinaryNumber[gapIndex + 1] === target)
-        {
+        while (gapIndex + 1 < aBinaryNumber.length && aBinaryNumber[gapIndex + 1] === target) {
             gapIndex++;
         }
-        if (gapIndex < aBinaryNumber.length){
-            return { response : true, gapIndexEnd : gapIndex + 1, gapIndexStart : index, target : target } 
+        if (gapIndex < aBinaryNumber.length) {
+            return { response: true, gapIndexEnd: gapIndex + 1, gapIndexStart: index, target: target }
         }
-        return { response : false, gapIndexEnd : gapIndex + 1, gapIndexStart : index, target : target }         
+        return { response: false, gapIndexEnd: gapIndex + 1, gapIndexStart: index, target: target }
     };
     var start = 0;
     var resultInterVal = [];
-    while (start < binaryNumber.length)
-    {
+    while (start < binaryNumber.length) {
         var gapInterval = findGap(start, binaryNumber, binaryNumber[start]);
         start = gapInterval.gapIndexEnd;
-        resultInterVal.push(gapInterval);   
+        if (gapInterval.target === '0') {
+            resultInterVal.push(gapInterval);
+        }
     }
 
-    var maxZeroSequence = resultInterVal.reduce(function(maxZeroSequence, gapInterval)
-    {
+    var maxZeroSequence = resultInterVal.reduce(function (maxZeroSequence, gapInterval) {
         if (gapInterval.target === "0") {
             if (gapInterval.gapIndexStart > 0 && gapInterval.gapIndexEnd < binaryNumber.length
-            )
-            {
+            ) {
                 var zeroSequenceLength = gapInterval.gapIndexEnd - gapInterval.gapIndexStart;
-                if (maxZeroSequence < zeroSequenceLength)
-                {
+                if (maxZeroSequence < zeroSequenceLength) {
                     maxZeroSequence = zeroSequenceLength;
                 }
             }
